83097e4a693055c210ee96fecd36755c9e47852c
WojciechWnek <wownek@gmail.com>
2025-03-07 13:17:27 +0100

    Add complete endpoint

--- drf_todo/api/views.py
+++ drf_todo/api/views.py
@@ -1,4 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from .models import Task
 from .serializers import TaskSerializer
 
@@ -6,3 +9,11 @@ from .serializers import TaskSerializer
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+    @action(detail=True, methods=["post"], parser_classes=[])
+    def complete(self, request, pk=None):
+        task = self.get_object()
+        task.completed = True
+        task.save()
+        return Response({'status': 'Task marked as completed'}, status=status.HTTP_200_OK)
+
--- drf_todo/drf_todo/settings.py
+++ drf_todo/drf_todo/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_spectacular',
+
     'api',
 ]
 
@@ -123,3 +125,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'DRF Todo',
+    'DESCRIPTION': 'API documentation',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
